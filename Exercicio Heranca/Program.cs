6a3e48c7e615a6e124cc0b232d92b19ee8f7f3fb
Felipe Batista <felipe.c.b@hotmail.com>
2021-11-03 19:13:17 -0300

    Inicio do exercicio

---
+++ Exercicio Heranca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio_Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
