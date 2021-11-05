da207fddd2dd00bc830c8785a44327da1e4b0f1d
Felipe Batista <felipe.c.b@hotmail.com>
2021-11-05 19:42:51 -0300

    New commit

--- Exercicio Heranca/Program.cs
+++ Exercicio Heranca/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Exercicio_Heranca.Entities;
+
 
 namespace Exercicio_Heranca
 {
@@ -6,7 +8,25 @@ namespace Exercicio_Heranca
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+
+            //Upcasting
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+
+            // Downcasting
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
+            BusinessAccount acc5 = (BusinessAccount)acc3;
+
+
         }
     }
 }
