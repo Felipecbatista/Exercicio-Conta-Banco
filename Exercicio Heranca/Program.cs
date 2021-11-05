0a171cf5d00ae81b1a898b2afe8d3b8c9a5f0f8f
Felipe Batista <felipe.c.b@hotmail.com>
2021-11-05 20:01:49 -0300

    Adicionado classe SavingsAccount

--- Exercicio Heranca/Program.cs
+++ Exercicio Heranca/Program.cs
@@ -8,25 +8,16 @@ namespace Exercicio_Heranca
     {
         static void Main(string[] args)
         {
-            Account acc = new Account(1001, "Alex", 0.0);
-            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+            Account acc1 = new Account(1001, "Alex", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Ana", 500.0, 0.01);
 
 
-            //Upcasting
 
-            Account acc1 = bacc;
-            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
-            Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+            acc1.Withdraw(10.0);
+            acc2.Withdraw(10.0);
 
-
-            // Downcasting
-
-            BusinessAccount acc4 = (BusinessAccount)acc2;
-            acc4.Loan(100.0);
-
-            BusinessAccount acc5 = (BusinessAccount)acc3;
-
-
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
         }
     }
 }
